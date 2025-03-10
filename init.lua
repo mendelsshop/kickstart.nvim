62498a8dd31a9e016abdd58bd3d51af9e0a71474
mendelsshop <mendelsshop@gmail.com>
2025-03-10 13:05:32 -0400

    Added an shortcut that goes to defintion in a split

--- init.lua
+++ init.lua
@@ -592,6 +592,10 @@ require('lazy').setup {
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-T>.
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gsd',function ()
+            vim.cmd.vs()
+            require('telescope.builtin').lsp_definitions()
+          end,  '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
